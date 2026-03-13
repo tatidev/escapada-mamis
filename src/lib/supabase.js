5ab2f6fe8ff1b43ddfda4772a01a4bf32255a710
vluna-yoga <lunavictor@gmail.com>
2026-03-13 19:00:39 -0300

    ini

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Usamos valores por defecto vacíos para evitar que el constructor de Supabase explote
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL || "";
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY || "";
+
+if (!supabaseUrl || !supabaseKey) {
+    console.error("⚠️ Error: Las variables de Supabase no están cargadas.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
